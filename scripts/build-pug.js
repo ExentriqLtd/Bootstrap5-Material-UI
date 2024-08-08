6c5478883b753442d6d2a4f2c60a743e4de2d413
fvenuto <fvenuto87@gmail.com>
2024-08-08 15:50:04 +0200

    Working pug navigation

--- scripts/build-pug.js
+++ scripts/build-pug.js
@@ -1,9 +1,17 @@
 const { execSync } = require('child_process');
 
-// Compila i file Pug nella cartella modules
+// Dati da passare ai template Pug
+const docRoute = {
+  root_path: '',
+  path: 'index',
+  name_section: '',
+  name_chapter: 'Home'
+};
+
+// Compila i file Pug
 try {
-    execSync('npx pug views --out public', { stdio: 'inherit' });
-    console.log('Pug files compiled successfully.');
+  execSync(`npx pug --obj '${JSON.stringify({ doc_route: docRoute })}' views/ -o public/ -P`, { stdio: 'inherit' });
+  console.log('Pug files compiled successfully.');
 } catch (error) {
   console.error('Error compiling Pug files:', error.message);
 }
--- server.js
+++ server.js
@@ -8,14 +8,25 @@ const PORT = process.env.PORT || 8000;
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
-// Servire file statici dalla cartella dist e public
+// Servire file statici dalla cartella public e dist
 app.use('/dist', express.static(path.join(__dirname, 'dist')));
 app.use(express.static(path.join(__dirname, 'public')));
 
-// Route principale
-app.get('/', (req, res) => {
-  res.render('layout', { title: 'My App' });
+// Middleware per estrarre i parametri di query
+app.use((req, res, next) => {
+  res.locals.query = req.query;
+  next();
 });
+
+// Route principale per servire index.pug
+app.get('/', (req, res) => {
+  res.render('index', { title: 'My App', query: req.query });
+});
+
+// Serve index.html for any other route
+// app.get('*', (req, res) => {
+//   res.sendFile(path.join(__dirname, 'public', 'index.html'));
+// });
 
 // Avvia il server
 app.listen(PORT, () => {
