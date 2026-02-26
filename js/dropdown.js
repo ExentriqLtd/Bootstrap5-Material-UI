66ca807989626d4982798da79a803937286909b0
fvenuto <fvenuto87@gmail.com>
2026-02-26 18:59:11 +0100

    chore(dropdown): use console.log for clamp diagnostics

--- js/dropdown.js
+++ js/dropdown.js
@@ -235,7 +235,7 @@ $.fn.dropdown = function (option) {
                 const overflowLeft = boundLeft - menuRect.left;
 
                 // Debug: helps verify real bounds used at runtime
-                console.debug('[Dropdown Clamp]', {
+                console.log('[Dropdown Clamp]', {
                     id: object.attr('id'),
                     menuLeft: menuRect.left,
                     menuRight: menuRect.right,
